3738f48eee44f6d200bf8fa7c0f8add53b6a658a
suman7490 <suman.dhangar.135497@gmail.com>
2024-12-16 14:23:08 +0530

    changed re.header origin

--- api/server.mjs
+++ api/server.mjs
@@ -51,7 +51,7 @@ db.getConnection(err => {
 });
 // *****************************************************************
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://quotation.kalpresearchwork.com');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
