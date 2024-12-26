77c371332085e33663787fc1e4a96536a534d4f5
suman7490 <suman.dhangar.135497@gmail.com>
2024-12-26 10:26:43 +0530

    commented res.headers

--- api/server.mjs
+++ api/server.mjs
@@ -41,13 +41,13 @@ db.getConnection(err => {
     console.log('Connected to MySQL Database');
 });
 // *****************************************************************
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://quotation.kalpresearchwork.com');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'https://quotation.kalpresearchwork.com');
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
 
 // ************** For getting all the routes ***********************
 app.use(express.static(path.join(__dirname, 'public_html')));
